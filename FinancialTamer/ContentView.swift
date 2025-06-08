baa15a6b091193458bafa8ca32fd5de314662ddb
IceRedline <scpd2604@gmail.com>
2025-06-08 20:50:09 +0300

    Initial Commit

---
+++ FinancialTamer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinancialTamer
+//
+//  Created by Артем Табенский on 08.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinancialTamer/FinancialTamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialTamerApp.swift
+//  FinancialTamer
+//
+//  Created by Артем Табенский on 08.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinancialTamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
